125e2cdf98de5b0ccd5782577cdaef5ecf880fd0
Felipe <feferreira.sf@gmail.com>
2023-01-20 00:24:19 -0300

    feat: home with reusable components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import HabitsSummary from './components/HabitsSummary'
+import Header from './components/Header'
+
 function App() {
   return (
-    <div className='mx-auto mt-4 w-full max-w-[1440px]'>
-      <h1 className='text-4xl font-extrabold text-purple-600'>Habit Tracker</h1>
+    <div className='flex h-screen w-screen flex-col items-center justify-center'>
+      <div className='flex w-full max-w-[1440px] flex-col gap-16 px-6'>
+        <Header />
+        <HabitsSummary />
+      </div>
     </div>
   )
 }
