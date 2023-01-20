233ff69230614735a9ef14e7f1ee318d9e4d96d6
Felipe <feferreira.sf@gmail.com>
2023-01-19 22:08:25 -0300

    style: added bg color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        background: '#09090A'
+      },
       fontFamily: {
         inter: ['Inter', 'sans-serif']
       }
